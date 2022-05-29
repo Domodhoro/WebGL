4d2bfc38a7db7f894d1ca3b21075f2842ddc34d7
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-29 14:50:13 -0300

    Update triangle.js

--- 1 - Triangle/triangle.js
+++ 1 - Triangle/triangle.js
@@ -1,14 +1,9 @@
-class Triangle
-{
-	constructor(gl, vertex, color)
-	{
-		this.vertex = vertex;
+class Triangle {
+	constructor(gl, vertex, color) {
+	    this.vertex = vertex;
 
-	    const vertexBuffer = gl.createBuffer();
-	    const colorBuffer = gl.createBuffer();
-
-	    const vertexCoords = gl.getAttribLocation(gl.program, 'vertexCoords');
-	    const colorCoords = gl.getAttribLocation(gl.program, 'colorCoords');
+	    const vertexBuffer = gl.createBuffer(), colorBuffer = gl.createBuffer();
+	    const vertexCoords = gl.getAttribLocation(gl.program, 'vertexCoords'), colorCoords = gl.getAttribLocation(gl.program, 'colorCoords');
 
 	    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 	    gl.bufferData(gl.ARRAY_BUFFER, this.vertex, gl.STATIC_DRAW);
@@ -21,8 +16,7 @@ class Triangle
 	    gl.enableVertexAttribArray(colorCoords);
 	}
 
-	getVertex = function()
-	{
-		return this.vertex.length / 3;
+	getVertex = function() {
+	    return this.vertex.length / 3;
 	}
 }
